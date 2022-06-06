3820d8d3e18696f28c8ac2483b5ae5ce00e58f01
salineselin <salineselin@gmail.com>
2022-06-05 20:30:13 -0600

    added base infrastructure as code for pulumi

---
+++ pulumi/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/storage"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create a GCP resource (Storage Bucket)
+		_, err := storage.NewBucket(ctx, "my-bucket", &storage.BucketArgs{
+			Location: pulumi.String("US"),
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
