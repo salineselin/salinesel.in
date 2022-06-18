d8869fb805693e0dd52716028a0bfcb4c22dcbd3
salineselin <salineselin@gmail.com>
2022-06-17 23:44:09 -0600

    checking in basic structure of site with underlying gcp resources

--- pulumi/main.go
+++ pulumi/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 
+	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/compute"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/organizations"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/projects"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/serviceaccount"
@@ -17,13 +19,32 @@ func main() {
 		if err != nil {
 			return err
 		}
-		// remove projects/ prefix on project id
-		projectId := strings.TrimPrefix(project.Id, "projects/")
 
-		// Create a GCP resource (Storage Bucket)
-		bucket, err := storage.NewBucket(ctx, "salinesel-in", &storage.BucketArgs{
+		// define some common attributes with an anonymous struct
+		base := "salinesel-in"
+		domain := "salinesel.in"
+		site := struct {
+			bucketName         string
+			backendBucketName  string
+			serviceaccountName string
+			domain             string
+			apexDomain         string
+			TopLevelDomain     string
+			projectId          string
+		}{
+			bucketName:         base,
+			backendBucketName:  base + "-backendbucket",
+			serviceaccountName: base + "-web",
+			domain:             domain,
+			apexDomain:         strings.Split(domain, ".")[0],
+			TopLevelDomain:     strings.Split(domain, ".")[1],
+			projectId:          strings.TrimPrefix(project.Id, "projects/"), // remove projects/ prefix on project id
+		}
+
+		// create storage bucket
+		bucket, err := storage.NewBucket(ctx, site.bucketName, &storage.BucketArgs{
 			Location:                 pulumi.String("US"),
-			Name:                     pulumi.String("salinesel-in"),
+			Name:                     pulumi.String(site.bucketName),
 			ForceDestroy:             pulumi.Bool(true),
 			UniformBucketLevelAccess: pulumi.Bool(true),
 			Website: &storage.BucketWebsiteArgs{
@@ -41,8 +62,8 @@ func main() {
 						pulumi.String("DELETE"),
 					},
 					Origins: pulumi.StringArray{
-						pulumi.String("http://salinesel.in"),
-						pulumi.String("https://salinesel.in"),
+						pulumi.String("http://" + site.domain),
+						pulumi.String("https://" + site.domain),
 					},
 					ResponseHeaders: pulumi.StringArray{
 						pulumi.String("*"),
@@ -55,7 +76,8 @@ func main() {
 		}
 
 		// allow people online to view the contents of the bucket
-		_, err = storage.NewBucketIAMBinding(ctx, "salinesel-in-bucket-iambinding", &storage.BucketIAMBindingArgs{
+		name := fmt.Sprintf("%s-bucket-iambinding", site.bucketName)
+		_, err = storage.NewBucketIAMBinding(ctx, name, &storage.BucketIAMBindingArgs{
 			Bucket: bucket.Name,
 			Role:   pulumi.String("roles/storage.objectViewer"),
 			Members: pulumi.StringArray{
@@ -82,11 +104,102 @@ func main() {
 			}
 		}
 
+		// create a backend bucket associated with the storage bucket
+		name = site.backendBucketName
+		backendBucket, err := compute.NewBackendBucket(ctx, name, &compute.BackendBucketArgs{
+			Name:       pulumi.String(site.backendBucketName),
+			BucketName: pulumi.String(site.bucketName),
+			EnableCdn:  pulumi.Bool(true),
+		})
+		if err != nil {
+			return err
+		}
+
+		// create a health check
+		name = fmt.Sprintf("%s-healthcheck", site.domain)
+		healthCheck, err := compute.NewHttpHealthCheck(ctx, name, &compute.HttpHealthCheckArgs{
+			RequestPath:      pulumi.String("/"),
+			CheckIntervalSec: pulumi.Int(1),
+			TimeoutSec:       pulumi.Int(1),
+		})
+		if err != nil {
+			return err
+		}
+
+		// create a http backendservice
+		name = fmt.Sprintf("%s-backendservice", site.domain)
+		backendService, err := compute.NewBackendService(ctx, name, &compute.BackendServiceArgs{
+			PortName:     pulumi.String("http"),
+			Protocol:     pulumi.String("HTTP"),
+			TimeoutSec:   pulumi.Int(10),
+			HealthChecks: healthCheck.ID(),
+		})
+		if err != nil {
+			return err
+		}
+
+		// create url map for backend bucket
+		// https://www.pulumi.com/registry/packages/gcp/api-docs/compute/targethttpsproxy/
+		name = fmt.Sprintf("%s-urlmap", site.domain)
+		urlmap, err := compute.NewURLMap(ctx, name, &compute.URLMapArgs{
+			DefaultService: backendBucket.ID(),
+			HostRules: compute.URLMapHostRuleArray{
+				&compute.URLMapHostRuleArgs{
+					Hosts: pulumi.StringArray{
+						pulumi.String(site.apexDomain),
+					},
+					PathMatcher: pulumi.String(site.apexDomain),
+				},
+			},
+			PathMatchers: compute.URLMapPathMatcherArray{
+				&compute.URLMapPathMatcherArgs{
+					Name:           pulumi.String(site.apexDomain),
+					DefaultService: backendService.ID(),
+					PathRules: compute.URLMapPathMatcherPathRuleArray{
+						&compute.URLMapPathMatcherPathRuleArgs{
+							Service: backendService.ID(),
+							Paths: pulumi.StringArray{
+								pulumi.String("/*"),
+							},
+						},
+					},
+				},
+			},
+			DefaultUrlRedirect: compute.URLMapDefaultUrlRedirectArgs{
+				StripQuery:    pulumi.Bool(false),
+				HttpsRedirect: pulumi.Bool(false),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// create an SSL certificate to encrypt the frontend
+		name = fmt.Sprintf("%s-ssl-cert", site.domain)
+		sslCert, err := compute.NewSSLCertificate(ctx, name, &compute.SSLCertificateArgs{
+			NamePrefix: pulumi.String(site.bucketName),
+		})
+		if err != nil {
+			return err
+		}
+
+		// bind the https certificate to a load balancer
+		name = fmt.Sprintf("%s-https-proxy", site.domain)
+		_, err = compute.NewTargetHttpsProxy(ctx, name, &compute.TargetHttpsProxyArgs{
+			UrlMap: urlmap.ID(),
+			SslCertificates: pulumi.StringArray{
+				sslCert.ID(),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
 		// Create a serviceaccount
-		saName := "salinesel-in-web"
-		sa, err := serviceaccount.NewAccount(ctx, saName, &serviceaccount.AccountArgs{
-			AccountId:   pulumi.String(saName),
-			DisplayName: pulumi.String(saName),
+		name = site.serviceaccountName
+		sa, err := serviceaccount.NewAccount(ctx, name, &serviceaccount.AccountArgs{
+			AccountId:   pulumi.String(name),
+			DisplayName: pulumi.String(name),
 			Description: pulumi.String("ServiceAccount used by Github Actions for seeding and serving content from the website bucket"),
 		}, pulumi.Protect(true))
 		if err != nil {
@@ -99,19 +212,19 @@ func main() {
 		}).(pulumi.StringOutput)
 
 		// make the serviceaccount a workload identity user
-		_, err = serviceaccount.NewIAMBinding(ctx, "make-sa-workload-identity-user", &serviceaccount.IAMBindingArgs{
+		name = fmt.Sprintf("make-%s-workload-identity-user", site.serviceaccountName)
+		_, err = serviceaccount.NewIAMMember(ctx, name, &serviceaccount.IAMMemberArgs{
 			ServiceAccountId: sa.Name,
 			Role:             pulumi.String("roles/iam.workloadIdentityUser"),
-			Members: pulumi.StringArray{
-				pulumi.String("serviceAccount:" + projectId + ".svc.id.goog[salinesel-in/salinesel-in]"),
-			},
+			Member:           pulumi.Sprintf("serviceAccount:%s.svc.id.goog[salinesel-in/salinesel-in]", site.projectId),
 		})
 		if err != nil {
 			return err
 		}
 
 		// give the serviceaccount permissions to read and write to the website bucket for CI
-		_, err = storage.NewBucketIAMMember(ctx, "give-sa-bucket-permissions", &storage.BucketIAMMemberArgs{
+		name = fmt.Sprintf("give-%s-bucket-rw", site.serviceaccountName)
+		_, err = storage.NewBucketIAMMember(ctx, name, &storage.BucketIAMMemberArgs{
 			Bucket: bucket.Name,
 			Role:   pulumi.String("roles/storage.admin"),
 			Member: saWithPrefix,
@@ -121,20 +234,20 @@ func main() {
 		}
 
 		// give the serviceaccount permissions to create and remove DNS records in the salinesel.in domain
-		_, err = projects.NewIAMMember(ctx, "give-sa-dns-rw", &projects.IAMMemberArgs{
-			Project: pulumi.String(projectId),
+		name = fmt.Sprintf("give-%s-dns-rw", site.serviceaccountName)
+		_, err = projects.NewIAMMember(ctx, name, &projects.IAMMemberArgs{
+			Project: pulumi.String(site.projectId),
 			Role:    pulumi.String("roles/dns.admin"),
 			Member:  saWithPrefix,
 			Condition: &projects.IAMMemberConditionArgs{
-				Description: pulumi.String("only for the salinesel.in zone"),
-				Title:       pulumi.String("only-salinesel.in"),
-				Expression:  pulumi.String("resource.name == \"salinesel-in\""),
+				Description: pulumi.Sprintf("only for the %s zone", site.domain),
+				Title:       pulumi.Sprintf("only-%s", site.domain),
+				Expression:  pulumi.Sprintf("resource.name == \"%s\"", site.bucketName),
 			},
 		})
 		if err != nil {
 			return err
 		}
-
 		return nil
 	})
 }
