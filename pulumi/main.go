536faa448cade9120efd63acad0eeb07b9326938
salineselin <salineselin@gmail.com>
2022-06-23 22:11:03 -0600

    removed other pulumi resources

--- pulumi/main.go
+++ pulumi/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/organizations"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/projects"
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/serviceaccount"
-	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/storage"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -39,60 +38,10 @@ func main() {
 			projectId:          strings.TrimPrefix(project.Id, "projects/"), // remove projects/ prefix on project id
 		}
 
-		// create storage bucket
-		name := fmt.Sprintf("%s-bucket", site.bucketName)
-		bucket, err := storage.NewBucket(ctx, name, &storage.BucketArgs{
-			Location:                 pulumi.String("US"),
-			Name:                     pulumi.String(name),
-			ForceDestroy:             pulumi.Bool(true),
-			UniformBucketLevelAccess: pulumi.Bool(true),
-			Website: &storage.BucketWebsiteArgs{
-				MainPageSuffix: pulumi.String("index.html"),
-				NotFoundPage:   pulumi.String("404.html"),
-			},
-			Cors: storage.BucketCorArray{
-				&storage.BucketCorArgs{
-					MaxAgeSeconds: pulumi.Int(3600),
-					Methods: pulumi.StringArray{
-						pulumi.String("GET"),
-						pulumi.String("HEAD"),
-						pulumi.String("PUT"),
-						pulumi.String("POST"),
-						pulumi.String("DELETE"),
-					},
-					Origins: pulumi.StringArray{
-						pulumi.String("http://" + site.domain),
-						pulumi.String("https://" + site.domain),
-					},
-					ResponseHeaders: pulumi.StringArray{
-						pulumi.String("*"),
-					},
-				},
-			},
-		})
-		if err != nil {
-			return err
-		}
-
-		// allow people online to view the contents of the bucket
-		name = fmt.Sprintf("%s-bucket-iambinding", site.bucketName)
-		_, err = storage.NewBucketIAMBinding(ctx, name, &storage.BucketIAMBindingArgs{
-			Bucket: bucket.Name,
-			Role:   pulumi.String("roles/storage.objectViewer"),
-			Members: pulumi.StringArray{
-				pulumi.String("allUsers"),
-			},
-		})
-		if err != nil {
-			return err
-		}
-
 		// get the existing salinesel.in zone
-
 		// create a dns record mapping the ip address to salinesel.in
-
 		// Create a serviceaccount
-		name = site.serviceaccountName
+		name := site.serviceaccountName
 		sa, err := serviceaccount.NewAccount(ctx, name, &serviceaccount.AccountArgs{
 			AccountId:   pulumi.String(name),
 			DisplayName: pulumi.String(name),
@@ -113,17 +62,6 @@ func main() {
 			ServiceAccountId: sa.Name,
 			Role:             pulumi.String("roles/iam.workloadIdentityUser"),
 			Member:           pulumi.Sprintf("serviceAccount:%s.svc.id.goog[salinesel-in/salinesel-in]", site.projectId),
-		})
-		if err != nil {
-			return err
-		}
-
-		// give the serviceaccount permissions to read and write to the website bucket for CI
-		name = fmt.Sprintf("give-%s-bucket-rw", site.serviceaccountName)
-		_, err = storage.NewBucketIAMMember(ctx, name, &storage.BucketIAMMemberArgs{
-			Bucket: bucket.Name,
-			Role:   pulumi.String("roles/storage.admin"),
-			Member: saWithPrefix,
 		})
 		if err != nil {
 			return err
