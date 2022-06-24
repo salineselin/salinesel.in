84a622f0c090b71e58be197df10ffdc3d6fb740a
salineselin <salineselin@gmail.com>
2022-06-23 22:15:00 -0600

    simplified pulumi syntax by removing struct

--- pulumi/main.go
+++ pulumi/main.go
@@ -20,31 +20,16 @@ func main() {
 
 		// define some common attributes with an anonymous struct
 		base := "salinesel-in"
-		domain := "salinesel.in"
-		site := struct {
-			bucketName         string
-			backendBucketName  string
-			serviceaccountName string
-			domain             string
-			apexDomain         string
-			TopLevelDomain     string
-			projectId          string
-		}{
-			bucketName:         base,
-			serviceaccountName: base + "-web",
-			domain:             domain,
-			apexDomain:         strings.Split(domain, ".")[0],
-			TopLevelDomain:     strings.Split(domain, ".")[1],
-			projectId:          strings.TrimPrefix(project.Id, "projects/"), // remove projects/ prefix on project id
-		}
+		// domain := "salinesel.in"
+		serviceaccountname := base + "-web"
+		projectId := strings.TrimPrefix(project.Id, "projects/") // remove projects/ prefix on project id
 
 		// get the existing salinesel.in zone
 		// create a dns record mapping the ip address to salinesel.in
 		// Create a serviceaccount
-		name := site.serviceaccountName
-		sa, err := serviceaccount.NewAccount(ctx, name, &serviceaccount.AccountArgs{
-			AccountId:   pulumi.String(name),
-			DisplayName: pulumi.String(name),
+		sa, err := serviceaccount.NewAccount(ctx, serviceaccountname, &serviceaccount.AccountArgs{
+			AccountId:   pulumi.String(serviceaccountname),
+			DisplayName: pulumi.String(serviceaccountname),
 			Description: pulumi.String("ServiceAccount used by Github Actions for seeding and serving content from the website bucket"),
 		}, pulumi.Protect(true))
 		if err != nil {
@@ -57,27 +42,22 @@ func main() {
 		}).(pulumi.StringOutput)
 
 		// make the serviceaccount a workload identity user
-		name = fmt.Sprintf("give-%s-workload-identity-user", site.serviceaccountName)
+		name := fmt.Sprintf("give-%s-workload-identity-user", serviceaccountname)
 		_, err = serviceaccount.NewIAMMember(ctx, name, &serviceaccount.IAMMemberArgs{
 			ServiceAccountId: sa.Name,
 			Role:             pulumi.String("roles/iam.workloadIdentityUser"),
-			Member:           pulumi.Sprintf("serviceAccount:%s.svc.id.goog[salinesel-in/salinesel-in]", site.projectId),
+			Member:           pulumi.Sprintf("serviceAccount:%s.svc.id.goog[salinesel-in/salinesel-in]", projectId),
 		})
 		if err != nil {
 			return err
 		}
 
 		// give the serviceaccount permissions to create and remove DNS records in the salinesel.in domain
-		name = fmt.Sprintf("give-%s-dns-rw", site.serviceaccountName)
+		name = fmt.Sprintf("give-%s-dns-rw", serviceaccountname)
 		_, err = projects.NewIAMMember(ctx, name, &projects.IAMMemberArgs{
-			Project: pulumi.String(site.projectId),
+			Project: pulumi.String(projectId),
 			Role:    pulumi.String("roles/dns.admin"),
 			Member:  saWithPrefix,
-			Condition: &projects.IAMMemberConditionArgs{
-				Description: pulumi.Sprintf("only for the %s zone", site.domain),
-				Title:       pulumi.Sprintf("only-%s", site.bucketName),
-				Expression:  pulumi.Sprintf("resource.name == \"%s\"", site.bucketName),
-			},
 		})
 		if err != nil {
 			return err
